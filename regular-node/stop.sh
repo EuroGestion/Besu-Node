751e91dbc3743b5a8a1a5f85d174c2af584e6340
cfuente <cfuente@gmx.es>
2021-01-18 17:36:03 +0100

    Subida inicial según está en 5.153.57.78

---
+++ regular-node/stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set the BESU_P2P_HOST environment variable to the public IP address of your node
+export BESU_P2P_HOST=`dig +short myip.opendns.com @resolver1.opendns.com 2>/dev/null || curl -s --retry 2 icanhazip.com`
+
+# Start containers
+docker-compose down
